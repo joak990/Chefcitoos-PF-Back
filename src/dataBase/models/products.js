9f61e8e888b209f9f45764a063e555c884d2cf98
AndresGomez <yosoyandresfelipe@gmail.com>
2023-05-31 20:59:00 -0300

    the relationships of the tables products and order_products were modified.

--- src/dataBase/models/products.js
+++ src/dataBase/models/products.js
@@ -12,6 +12,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Products.hasMany(models.Order_product)
       Products.belongsToMany(models.components_categ,{ 
         through : "components_categ_products"
       })
