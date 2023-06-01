d507f935344c9c0af6f28a824730c8c295ec39a9
Alexanderfhg <0freddyherrera0@gmail.com>
2023-06-01 12:48:02 -0500

    Bug fixed

--- src/dataBase/migrations/20230529213113-create-users.js
+++ src/dataBase/migrations/20230529213113-create-users.js
@@ -19,7 +19,7 @@ module.exports = {
       },
       password: {
         type: Sequelize.STRING,
-        allowNull : false
+        allowNull : true
       },
       type: {
         type: Sequelize.STRING,
