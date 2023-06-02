b4ea1c2e83dd952729e2acdb3bf6334303a7368b
AndresGomez <yosoyandresfelipe@gmail.com>
2023-06-02 17:17:55 -0300

    apply filter in creations route, filter by user id, by creation id, order by date of creation and filter by posted true

---
+++ src/controllers/getCreationByUserId.js
@@ -0,0 +1,48 @@
+const { Creations } = require('../dataBase/models');
+const { Users } = require('../dataBase/models');
+const { products } = require('../dataBase/models');
+
+const getCreationsByUserId = async (id, type) => {
+    try {
+        let aux;
+        if (type === "creation") {
+            aux = await Creations.findByPk(id, {
+                order: [['createdAt', 'DESC']],
+                include: [
+                    {
+                        model: Users,
+                        as: 'Users',
+                        attributes: ['name']
+                    },
+                    {
+                        model: products,
+                        attributes: ['name']
+                    }
+                ]
+            });
+        } else if (type === "user") {
+            aux = await Creations.findAll({
+                where: {
+                    users_id: id,
+                },
+                order: [['createdAt', 'DESC']],
+                include: [
+                    {
+                        model: Users,
+                        as: 'Users',
+                        attributes: ['name']
+                    },
+                    {
+                        model: products,
+                        attributes: ['name']
+                    }
+                ]
+            });
+        }
+        return aux;
+    } catch (error) {
+        throw new Error(error);
+    }
+}
+
+module.exports = getCreationsByUserId;
