d94ac4c2faacda77e98150334ccbcdfbc9036e48
Alexanderfhg <0freddyherrera0@gmail.com>
2023-06-02 11:03:22 -0500

    order products changed

--- src/controllers/getProducts.js
+++ src/controllers/getProducts.js
@@ -3,7 +3,8 @@ const { products } = require('../dataBase/models');
 const getProducts = async () => {
     try {
         const allProducts = await products.findAll();
-        const aux = allProducts.map(el=>el.dataValues)
+        const aux = allProducts.map(el => el.dataValues);
+        aux.sort((a, b) => a.name.localeCompare(b.name));
         return aux;
     } catch (error) {
         throw new Error(error);
