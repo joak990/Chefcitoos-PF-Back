cc298162908e249e007d86d3c93058fa9bd568f3
AndresGomez <yosoyandresfelipe@gmail.com>
2023-06-03 18:57:02 -0300

    filters by price order and type product

--- src/controllers/getCreations.js
+++ src/controllers/getCreations.js
@@ -1,3 +1,5 @@
+const { Op } = require('sequelize');
+
 const { Creations } = require('../dataBase/models');
 const { Users } = require('../dataBase/models');
 const { products } = require('../dataBase/models');
@@ -12,6 +14,128 @@ const getCreations = async (filterName) => {
                     isPosted: true,
                 },
                 order: [['createdAt', 'DESC']],
+                include: [
+                    {
+                        model: Users,
+                        as: 'Users',
+                        attributes: ['name']
+                    },
+                    {
+                        model: products,
+                        attributes: ['name']
+                    }
+                ]
+            });
+            aux = creationPosted.map(el => el.dataValues)
+            return aux
+        } else if (filterName === "typeProductBurgers") {
+            const creationPosted = await Creations.findAll({
+                where: {
+                    product_id: {
+                        [Op.in]: [1, 2, 3, 4]
+                    }
+                },
+                order: [['createdAt', 'DESC']],
+                include: [
+                    {
+                        model: Users,
+                        as: 'Users',
+                        attributes: ['name']
+                    },
+                    {
+                        model: products,
+                        attributes: ['name']
+                    }
+                ]
+            });
+            aux = creationPosted.map(el => el.dataValues)
+            return aux
+        } else if (filterName === "typeProductHotDogs") {
+            const creationPosted = await Creations.findAll({
+                where: {
+                    product_id: {
+                        [Op.in]: [5, 6, 7, 8]
+                    }
+                },
+                order: [['createdAt', 'DESC']],
+                include: [
+                    {
+                        model: Users,
+                        as: 'Users',
+                        attributes: ['name']
+                    },
+                    {
+                        model: products,
+                        attributes: ['name']
+                    }
+                ]
+            });
+            aux = creationPosted.map(el => el.dataValues)
+            return aux
+        } else if (filterName === "typeProductSandwitch") {
+            const creationPosted = await Creations.findAll({
+                where: {
+                    product_id: {
+                        [Op.in]: [12, 13, 14, 15]
+                    }
+                },
+                order: [['createdAt', 'DESC']],
+                include: [
+                    {
+                        model: Users,
+                        as: 'Users',
+                        attributes: ['name']
+                    },
+                    {
+                        model: products,
+                        attributes: ['name']
+                    }
+                ]
+            });
+            aux = creationPosted.map(el => el.dataValues)
+            return aux
+        } else if (filterName === "typeProductBurrito") {
+            const creationPosted = await Creations.findAll({
+                where: {
+                    product_id: {
+                        [Op.in]: [9, 10, 11]
+                    }
+                },
+                order: [['createdAt', 'DESC']],
+                include: [
+                    {
+                        model: Users,
+                        as: 'Users',
+                        attributes: ['name']
+                    },
+                    {
+                        model: products,
+                        attributes: ['name']
+                    }
+                ]
+            });
+            aux = creationPosted.map(el => el.dataValues)
+            return aux
+        } else if (filterName === "priceDesc") {
+            const creationPosted = await Creations.findAll({
+                order: [['price', 'DESC']],
+                include: [
+                    {
+                        model: Users,
+                        as: 'Users',
+                        attributes: ['name']
+                    },
+                    {
+                        model: products,
+                        attributes: ['name']
+                    }
+                ]
+            });
+            aux = creationPosted.map(el => el.dataValues)
+            return aux
+        } else if (filterName === "priceAsc") {
+            const creationPosted = await Creations.findAll({
+                order: [['price', 'ASC']],
                 include: [
                     {
                         model: Users,
