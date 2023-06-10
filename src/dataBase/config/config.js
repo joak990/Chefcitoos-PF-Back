ad21749474b4a67350978f6fa4afbe3df9839c01
AndresGomez <yosoyandresfelipe@gmail.com>
2023-06-09 22:40:41 -0300

    changue files for deploy

--- src/dataBase/config/config.js
+++ src/dataBase/config/config.js
@@ -1,9 +1,15 @@
+require('dotenv').config();
+const { DB_USER,
+  DB_PASSWORD,
+  DB_NAME,
+  DB_HOST } = process.env
+
 module.exports = {
   "development": {
-    "username": "awschefcitoos",
-    "password": "chefcitoosdb",
-    "database": "chefcitoos",
-    "host": "database-1.cqawfiwiwudi.us-east-2.rds.amazonaws.com",
+   "username":DB_USER,
+    "password":DB_PASSWORD,
+    "database":DB_NAME,
+    "host":DB_HOST,
     "dialect": "postgres"
   },
   "test": {
@@ -14,10 +20,10 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username":DB_USER,
+    "password":DB_PASSWORD,
+    "database":DB_NAME,
+    "host":DB_HOST,
     "dialect": "mysql"
   }
 }
