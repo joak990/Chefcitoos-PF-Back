24338bca41150a25ecb67b18706779c73a712943
AndresGomez <yosoyandresfelipe@gmail.com>
2023-06-09 22:50:21 -0300

    changue for deploy

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+require('dotenv').config();
+
 const server = require('./src/server');
 
+server.get("/", (req, res) => {
+    res.send ({ msg: "Hola Chefcitoos"})
+  })
 
-
-server.listen(3001, () => {
-    console.log("Listening in port", 3001)
+server.listen(process.env.PORT, () => {
+    console.log("Listening in port", process.env.PORT)
 })
