38fe520701a0d5be4507ef053cf26785f5e6b0d7
Alexanderfhg <0freddyherrera0@gmail.com>
2023-06-18 00:23:04 -0500

    firebase storage part1

--- src/controllers/createProduct.js
+++ src/controllers/createProduct.js
@@ -1,7 +1,32 @@
 const { products } = require('../dataBase/models');
+const admin = require('firebase-admin');
+const serviceAccount = require('../../chefcitoosapp-6371a-firebase-adminsdk-d5c0l-89458ddbc6.json');
+
 
 const createProduct = async (product) => {
     try {
+        admin.initializeApp({
+            credential: admin.credential.cert(serviceAccount),
+            storageBucket: 'chefcitoosapp-6371a.appspot.com'
+        });
+
+        const bucket = admin.storage().bucket();
+        // console.log(bucket);
+        const localFilePath = 'C:/Users/0fred/Documents/chefcitoos-img/salchipapa-casa.jpg';
+        const storageFilePath = 'deditos.jpg'
+        const uploadResponse = await bucket.upload(localFilePath, {
+            destination: storageFilePath
+        })
+
+        const file = uploadResponse[0];
+        const imageUrl = await file.getSignedUrl({
+            action: 'read',   
+            expires: '03-01-2500'         
+        });
+
+        console.log('URL de la imagen:', imageUrl[0]);
+        return imageUrl[0];
+
         const newProduct = await products.create({
             name: product.name,
             description: product.description,
@@ -9,9 +34,11 @@ const createProduct = async (product) => {
             image: product.image,
             customizable: product.customizable,
             type_product: product.type_product,
-            isDeleted: product.isDeleted
+            isDeleted: product.isDeleted,
+            elements: product.elements
         })
         return newProduct.dataValues;
+        return true
     } catch (error) {
         throw new Error(error);
     }
